df536ea821835de017624786d991eb2c402dccd0
Nurbakyt Madibek <madibek.n@chocolife.kz>
2021-07-19 18:55:05 +0600

    update favicon files

--- components/layout.js
+++ components/layout.js
@@ -14,21 +14,21 @@ export default function Layout({ children, home }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         <meta
           name="description"
